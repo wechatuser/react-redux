6988fb6044a9eda987e07c9aa012705966c77e58
wechatuser <56934041+wechatuser@users.noreply.github.com>
2019-12-10 14:38:08 -0500

    Add files via upload

---
+++ src/TodoList.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+const TodoList = (props) => {
+	const { inputValue, changeInputValue, handleClick, list, handleDelete } = props;
+
+	return (
+		<div>
+			<div> 
+				<input value={inputValue} onChange={changeInputValue}/>
+				<button onClick={handleClick}>Submit</button>
+			</div>
+			<ul>
+				{
+					list.map((item, index) => {
+						return <li onClick={handleDelete} key{index}>{item}</li>
+					})
+				}
+			</ul>
+		</div>
+	)
+
+}
+
+const mapStateToProps = (state) => {
+	return {
+		inputValue: state.inputValue,
+		list: state.list
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		changeInputValue(e) {
+			const action = {
+				type: 'change_input_value',
+				value : e.target.value
+			};
+			dispatch(action);
+		},
+
+		handleClick() {
+			const action = {
+				type: 'add_item'
+			};
+			dispatch(action);
+		}
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
